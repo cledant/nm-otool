0b68adec21bb00bc57fa10e68ed39586f879bb6f
cledant <cledant@student.42.fr>
2017-01-25 13:19:03 +0100

    first push otool

---
+++ includes/otool/otool.h
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   otool.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cledant <cledant@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/01/25 13:08:15 by cledant           #+#    #+#             */
+/*   Updated: 2017/01/25 13:14:22 by cledant          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef OTOOL_H
+# define OTOOL_H
+
+# include "otool_includes.h"
+# include "otool_defines.h"
+# include "otool_functions.h"
+
+#endif
---
+++ includes/otool/otool_includes.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   otool_includes.h                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cledant <cledant@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/01/25 13:15:45 by cledant           #+#    #+#             */
+/*   Updated: 2017/01/25 13:17:03 by cledant          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef OTOOL_INCLUDES_H
+# define #OTOOL_INCLUDES_H
+
+# include <sys/mman.h>
+# include <fcntl.h>
+# include <sys/stat.h>
+# include "libft.h"
+
+#endif
