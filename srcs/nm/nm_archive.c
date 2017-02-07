08d94c546c3a9eab122d9371af61d1df017279e9
cledant <cledant@student.42.fr>
2017-02-07 18:40:46 +0100

    fix for .a malformed

--- srcs/nm/nm_archive.c
+++ srcs/nm/nm_archive.c
@@ -6,7 +6,7 @@
 /*   By: cledant <cledant@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/02/03 22:29:34 by cledant           #+#    #+#             */
-/*   Updated: 2017/02/04 22:13:41 by cledant          ###   ########.fr       */
+/*   Updated: 2017/02/07 18:39:07 by cledant          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,14 +18,13 @@ static int		init_archive_explore(const t_info *info,
 	size_t		header_size;
 	size_t		nb_item;
 
-	if ((header_size = nm_get_header_size(start_header)) == 0)
-		return (nm_error_handler(ERR_INVALID_ARCHIVE));
+	header_size = nm_get_header_size(start_header);
 	if (nm_is_interval_valid((size_t)start_header + sizeof(struct ar_hdr),
 			header_size, info) == NM_FAIL)
 		return (nm_error_handler(ERR_INVALID_FILE));
 	if ((nb_item = nm_get_nb_item((void *)start_header + ex_size +
 			sizeof(struct ar_hdr))) == 0)
-		return (nm_error_handler(ERR_INVALID_ARCHIVE));
+		nb_item = 2;
 	nm_archive_explore((void *)start_header + sizeof(struct ar_hdr)
 		+ header_size, nb_item, info);
 	return (NM_OK);
--- srcs/otool/otool_archive.c
+++ srcs/otool/otool_archive.c
@@ -6,7 +6,7 @@
 /*   By: cledant <cledant@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/01/30 13:14:54 by cledant           #+#    #+#             */
-/*   Updated: 2017/02/01 12:59:55 by cledant          ###   ########.fr       */
+/*   Updated: 2017/02/07 18:40:08 by cledant          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,14 +18,13 @@ static int		init_archive_explore(const t_info *info,
 	size_t		header_size;
 	size_t		nb_item;
 
-	if ((header_size = otool_get_header_size(start_header)) == 0)
-		return (otool_error_handler(ERR_INVALID_ARCHIVE));
+	header_size = otool_get_header_size(start_header);
 	if (otool_is_interval_valid((size_t)start_header + sizeof(struct ar_hdr),
 			header_size, info) == OTOOL_FAIL)
 		return (otool_error_handler(ERR_INVALID_FILE));
 	if ((nb_item = otool_get_nb_item((void *)start_header + ex_size +
 			sizeof(struct ar_hdr))) == 0)
-		return (otool_error_handler(ERR_INVALID_ARCHIVE));
+		nb_item = 2;
 	otool_archive_explore((void *)start_header + sizeof(struct ar_hdr)
 		+ header_size, nb_item, info);
 	return (OTOOL_OK);
